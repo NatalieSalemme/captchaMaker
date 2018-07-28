3fb1e09ed9ae91ae2a3c99a13b3f4ead6fc1ff44
Natalie Salemme <nataliesalemme@gmail.com>
2018-07-28 14:19:12 -0700

    Adding refresh button

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ let captchaText = document.querySelector('#captcha');
 let userText = document.querySelector('#textBox');
 let submitButton = document.querySelector('#submit');
 let output = document.querySelector('#output');
+let refreshButton = document.querySelector('#refresh');
 
 
 let alphaNums = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // let random = alphaNums[Math.floor(Math.random() * alphaNums.length)];
+
 let emptyArr = [];
+
 for(let i = 1; i <= 7; i++) {
 	emptyArr.push(alphaNums[Math.floor(Math.random() * alphaNums.length)]);
 }
+
 captchaText.innerHTML = emptyArr.join('');
 
 
-// let newArr = [];
-// let newText = newArr.push(userText.value);
-// console.log(newArr);
+let newArr = [];
+let newText = newArr.push(userText.value);
+console.log(newArr);
 
 submitButton.addEventListener('click',  function() {
 	if(userText.value === captchaText.innerHTML) {
@@ -27,4 +31,13 @@ submitButton.addEventListener('click',  function() {
 		output.innerHTML = "Incorrect, please try again";
 	}
 });
-//refresh button
+
+refreshButton.addEventListener('click', function () {
+	userText.value = "";
+	let refreshArr = [];
+	for(let j = 1; j <= 7; j++) {
+		refreshArr.push(alphaNums[Math.floor(Math.random() * alphaNums.length)]);
+	}
+	captchaText.innerHTML = refreshArr.join('');
+	output.innerHTML = "";
+});
