e8d92e9be3df167e7aad03e0274afc3f8371a969
Natalie Salemme <NatalieSalemme@gmail.com>
2018-07-28 12:01:01 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let captchaText = document.querySelector('#captcha');
+
+
+
+
+let alphaNums = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// let random = alphaNums[Math.floor(Math.random() * alphaNums.length)];
+let emptyArr = [];
+for(let i = 1; i <= 7; i++) {
+	emptyArr.push(alphaNums[Math.floor(Math.random() * alphaNums.length)]);
+}
+console.log(emptyArr);
+captcha.innerHTML = emptyArr;
